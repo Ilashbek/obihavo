5ca4faf0c7cfb9cc8c4a7442ea85ef05230c1eab
Ilashbek <127771485+Ilashbek@users.noreply.github.com>
2023-04-29 03:18:07 -0700

    brinchi commit

---
+++ data/config.py
@@ -0,0 +1,4 @@
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = "6086632550:AAGWY2ZlCZ9P_MIzmgLOr78EAZycCkUgyII"  # Bot toekn
+ADMINS = [1914536119] # adminlar ro'yxati
+IP = "localhost"  # Xosting ip manzili
